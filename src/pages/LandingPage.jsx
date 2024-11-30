933bd33b071877e1e066eacf37c0b86ebe779d8c
soham203 <soham203patil@gmail.com>
2024-11-30 21:47:22 +0530

    Production ready for main project page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 import { Typewriter } from 'react-simple-typewriter';
 
 export default function LandingPage() {
+
+    const navigate = useNavigate();
+
+    const handelNav = () => {
+      navigate('/quizpage');
+    }
+
   return (
     
-    <div className='text-white'>
+    <div className='text-white bg-black'>
     <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
       <p className='text-[#8E44AD] font-bold p-2'>
       POWERING THE FUTURE OF WEB 3.0
@@ -29,7 +37,7 @@ export default function LandingPage() {
         </div>
       </div>
       <p className='md:text-2xl text-xl font-bold text-gray-500'> One-stop quiz site for all blockchain-related technologies.</p>
-      <button className='bg-[#8E44AD] w-[200px] rounded-md my-6 mx-auto py-3 text-black text-xl font-semibold'>Take Quiz</button>
+      <button onClick={ handelNav } className='bg-[#8E44AD] w-[200px] rounded-md my-6 mx-auto py-3 text-black text-xl font-semibold'>Take Quiz</button>
     </div>
   </div>
 );
